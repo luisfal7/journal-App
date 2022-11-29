5fbc1e66e9c79cccc10cbc3b42665b00c6fcd755
Luis Falconi <luism.falconi@gmail.com>
2022-11-29 10:40:50 -0300

    add module structure

---
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Dayboook layout</h1>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,17 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>hola mundo</h1>
-    <button class="btn btn-primary">primary</button>
+    <button @click="goToDayBook" class="btn btn-primary">primary</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    goToDayBook() {
+      this.$router.push({name:'daybook'})
+    }
+  }
+}
+</script>
+
