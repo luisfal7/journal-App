ce424f56140fde64b5aff14bdfc9b408674b4260
Luis Falconi <luism.falconi@gmail.com>
2022-11-29 17:56:25 -0300

    modif state store journal

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,23 @@
 export default () => ({
     isLoading: true,
-    entries: [1,2,3,4,5,6,7]
+    entries: [
+        {
+            id: new Date().getTime() + 1000, // 123454986
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'So how did the classical Latin become so incoherent? According to McClintock, a 15th century typesetter likely scrambled part of Ciceros De Finibus in order to provide placeholder text to mockup various fonts for a type specimen book.',
+            picture: null // https://
+        },
+        {
+            id: new Date().getTime() + 2000, // 123454986
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'So how did the classical Latin become so incoherent? According to McClintock, a 15th century typesetter likely scrambled part of Ciceros De Finibus in order to provide placeholder text to mockup various fonts for a type specimen book.',
+            picture: null // https://
+        },
+        {
+            id: new Date().getTime() + 3000, // 123454986
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'So how did the classical Latin become so incoherent? According to McClintock, a 15th century typesetter likely scrambled part of Ciceros De Finibus in order to provide placeholder text to mockup various fonts for a type specimen book.',
+            picture: null // https://
+        },
+    ]
 })
