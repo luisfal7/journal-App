27d89e4f529bd2dbf721d8a7ea766be7314b10e1
Luis Falconi <luism.falconi@gmail.com>
2022-11-29 14:41:15 -0300

    add comp. EntryView

--- src/modules/daybook/components/Entry.vue
+++ src/modules/daybook/components/Entry.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="entry-container mb-3 pointer p-2">
+  <div
+    class="entry-container mb-3 pointer p-2"
+    @click="$router.push({ name: 'entry', params: { id: 1 } })"
+  >
     <div class="entry-title d-flex">
       <span class="text-success fs-5 fw-bold">29</span>
       <span class="mx-1 fs-5">noviembre</span>
       <span class="mx-2 fw-light">2022, martes</span>
     </div>
     <div class="entry-description">
-        So how did the classical Latin become so incoherent? According to
-        McClintock, a 15th century typesetter likely scrambled part of Cicero's De
-        Finibus in order to provide placeholder text to mockup various fonts for a
-        type specimen book.
+      So how did the classical Latin become so incoherent? According to
+      McClintock, a 15th century typesetter likely scrambled part of Cicero's De
+      Finibus in order to provide placeholder text to mockup various fonts for a
+      type specimen book.
     </div>
   </div>
 </template>
