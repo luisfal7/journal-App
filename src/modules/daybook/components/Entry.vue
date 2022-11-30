f5524693b708ce922273768f776a1c29f8b341af
Luis Falconi <luism.falconi@gmail.com>
2022-11-30 09:38:57 -0300

    modif comp. Entry

--- src/modules/daybook/components/Entry.vue
+++ src/modules/daybook/components/Entry.vue
@@ -1,21 +1,51 @@
 <template>
   <div
     class="entry-container mb-3 pointer p-2"
-    @click="$router.push({ name: 'entry', params: { id: 1 } })"
+    @click="$router.push({ name: 'entry', params: { id: entry.id } })"
   >
     <div class="entry-title d-flex">
-      <span class="text-success fs-5 fw-bold">29</span>
-      <span class="mx-1 fs-5">noviembre</span>
-      <span class="mx-2 fw-light">2022, martes</span>
+      <span class="text-success fs-5 fw-bold">{{ day }}</span>
+      <span class="mx-1 fs-5">{{ month }}</span>
+      <span class="mx-2 fw-light">{{ yearDay }}</span>
     </div>
     <div class="entry-description">
-      So how did the classical Latin become so incoherent? According to
-      McClintock, a 15th century typesetter likely scrambled part of Cicero's De
-      Finibus in order to provide placeholder text to mockup various fonts for a
-      type specimen book.
+      {{ shortText }}
     </div>
   </div>
 </template>
+
+<script>
+
+  const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+  const days   = ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado']
+
+  export default {
+    props:{
+      entry: {
+        type: Object,
+        required: true
+      }
+    },
+    computed:{
+      shortText(){
+        return ( this.entry.text.length > 130 ) ? this.entry.text.substring(0,130) + '...' : this.entry.text
+      },
+      day(){
+        const date = new Date(this.entry.date)
+        return date.getDate()
+      },
+      month(){
+        const date = new Date(this.entry.date)
+        return months[date.getMonth()]
+      },
+      yearDay(){
+        const date = new Date(this.entry.date)
+        return `${date.getFullYear()},${days[date.getDay()]}`
+      }
+    }
+  }
+</script>
+
 
 <style lang="scss" scoped>
 .entry-container {
