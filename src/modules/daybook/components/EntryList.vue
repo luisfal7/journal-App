ec0431a364dc65e42158cc9abe2277e04b777737
Luis Falconi <luism.falconi@gmail.com>
2022-12-01 10:19:57 -0300

    add new id post db

--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -3,6 +3,15 @@
     <div class="px-2 pt-2">
       <input type="='text" class="form-control" placeholder="Buscar entrada" v-model="term"/>
     </div>
+
+    <div class="mt-2 d-flex flex-column">
+      <button @click="$router.push({ name:'entry', params: { id: 'new' } })" class="btn btn-primary mx-3">
+        <i class="fa fa-plus-circle"></i>
+        Nueva entrada
+      </button>
+
+    </div>
+
     <div class="entry-scrollarea">
       <Entry v-for="entry in entriesByTerm" :key="entry.id" :entry="entry" />
     </div>
--- src/modules/daybook/views/NoEntrySelected.vue
+++ src/modules/daybook/views/NoEntrySelected.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex justify-content-center">
     <h1 class="align-self-center">No hay nada seleccionado</h1>
-    <Fab/>
+    <Fab @on:click="createNewEntry"/>
   </div>
 </template>
 
@@ -12,6 +12,11 @@ export default {
   components: {
     Fab: defineAsyncComponent(() => import("../components/Fab.vue")),
   },
+  methods: {
+    createNewEntry() {
+      this.$router.push({name:'entry', params:{ id: 'new' }})
+    }
+  }
 };
 </script>
 
