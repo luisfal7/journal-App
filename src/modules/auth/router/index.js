e8ab8a432bb9b6a9c996fd68c8ab6be8b8b9d94e
Luis Falconi <luism.falconi@gmail.com>
2022-12-05 15:30:21 -0300

    add view auth

--- src/modules/auth/router/index.js
+++ src/modules/auth/router/index.js
@@ -4,7 +4,16 @@ export default {
     name: 'auth',
     component: () => import(/* webpackChunkName: "auth" */ '@/modules/auth/layouts/AuthLayout.vue'),
     children: [
-        
+        {
+            path:'',
+            name:'login',
+            component: () => import(/* webpackChunkName: "login" */ '@/modules/auth/view/Login.vue'),
+        },
+        {
+            path:'/register',
+            name:'register',
+            component: () => import(/* webpackChunkName: "register" */ '@/modules/auth/view/Register.vue'),
+        },
     ]
 
 }
