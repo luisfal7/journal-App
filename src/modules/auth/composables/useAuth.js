234bcc317d2826d9ccadef2a05e38bd774b59af2
Luis Falconi <luism.falconi@gmail.com>
2022-12-07 09:13:02 -0300

    add auth status

--- src/modules/auth/composables/useAuth.js
+++ src/modules/auth/composables/useAuth.js
@@ -1,3 +1,4 @@
+import { computed } from "vue";
 import { useStore } from "vuex";
 
 const useAuth = () => {
@@ -18,9 +19,20 @@ const useAuth = () => {
         return resp
     }
 
+    const checkAuthStatus = async () => {
+
+        const resp = await store.dispatch('auth/checkAuthentication')
+
+        return resp
+
+    }
+
     return {
         createUser,
-        loginUser
+        loginUser,
+        checkAuthStatus,
+
+        authStatus: computed(() => store.getters['auth/currentState'])
     }
 }
 
--- src/modules/auth/store/actions.js
+++ src/modules/auth/store/actions.js
@@ -33,7 +33,6 @@ export const signInUser = async ({commit}, user) => {
     try {
 
         const { data } = await authApi.post(':signInWithPassword', { email, password, returnSecureToken: true } )
-        console.log(data)
         const { displayName, idToken, refreshToken } = data
 
         user.name = displayName
@@ -46,4 +45,35 @@ export const signInUser = async ({commit}, user) => {
         return { ok: false, message: error.response.data.error.message }
     }
 
+}
+
+export const checkAuthentication = async ({ commit }) => {
+
+    const idToken = localStorage.getItem('idToken')
+    const refreshToken = localStorage.getItem('idToken')
+
+    if( !idToken ){
+        commit('logout')
+        return { ok: false, message: 'No hay token' }
+    }
+
+    try {
+
+        const { data } = await authApi.post(':lookup', { idToken })
+        //console.log(data.users[0])
+        const { displayName, email } = data.users[0]
+
+        const user = {
+            name: displayName,
+            email
+        }
+
+        commit('loginUser', { user,idToken, refreshToken })
+
+        return { ok: true }
+        
+    } catch (error) {
+        commit('logout')
+        return { ok: false, message: error.response.data.error.message }
+    }
 }
